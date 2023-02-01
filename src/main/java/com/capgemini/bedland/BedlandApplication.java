9417b11234695c96428906adc63efcf7f5fb948a
KarolinaZ-k <karolina_zagrodnik@wp.pl>
2023-02-01 14:27:30 +0100

    Project template

---
+++ src/main/java/com/capgemini/bedland/BedlandApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.bedland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedlandApplication.class, args);
+	}
+
+}
