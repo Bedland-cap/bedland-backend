0c967ac16396a79b8ad17e601ace972a7d320ffd
KarolinaZ-k <karolina_zagrodnik@wp.pl>
2023-04-12 13:46:36 +0000

    Merged in bugfix/BL-210-improving-user-loading-time-be (pull request #39)
    BL-210 correct implementation in UserDao
    
    * BL-210 correct implementation in UserDao
    
    
    Approved-by: Przemyslaw Szafranski

--- src/main/java/com/capgemini/bedland/security/UserDao.java
+++ src/main/java/com/capgemini/bedland/security/UserDao.java
@@ -1,7 +1,9 @@
 package com.capgemini.bedland.security;
 
-import com.capgemini.bedland.manager.internal.ManagerRepository;
-import com.capgemini.bedland.member.internal.MemberRepository;
+import com.capgemini.bedland.entities.ManagerEntity;
+import com.capgemini.bedland.entities.MemberEntity;
+import com.capgemini.bedland.repositories.ManagerRepository;
+import com.capgemini.bedland.repositories.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -23,40 +26,38 @@ public class UserDao {
     private ManagerRepository managerRepository;
     private final PasswordEncoder passwordEncoder;
 
-    private List<UserDetails> createUserList() {
-        List<UserDetails> usersList = new ArrayList<>(List.of(
-                User.withUsername("user")
-                    .password(passwordEncoder.encode("user"))
-                    .roles("USER")
-                    .build(),
-                User.withUsername("admin")
-                    .password(passwordEncoder.encode("admin"))
-                    .roles("ADMIN")
-                    .build(),
-                User.withUsername("manager")
-                    .password(passwordEncoder.encode("manager"))
-                    .roles("MANAGER")
-                    .build())
-        );
-        memberRepository.findAll()
-                        .forEach(member -> usersList.add(User.withUsername(member.getLogin())
-                                                             .password(passwordEncoder.encode(member.getPassword()))
-                                                             .roles("USER")
-                                                             .build()));
-        managerRepository.findAll()
-                         .forEach(member -> usersList.add(User.withUsername(member.getLogin())
-                                                              .password(passwordEncoder.encode(member.getPassword()))
-                                                              .roles("MANAGER")
-                                                              .build()));
+    private List<UserDetails> createUserList(String login) {
+        Optional<MemberEntity> member = memberRepository.findAll()
+                                                        .stream()
+                                                        .filter(user -> user.getLogin()
+                                                                  .equals(login)).findFirst();
+        Optional<ManagerEntity> manager = managerRepository.findAll()
+                                                 .stream()
+                                                 .filter(user -> user.getLogin()
+                                                                  .equals(login)).findFirst();
+        List<UserDetails> usersList = new ArrayList<>();
+        if(member.isPresent()){
+            usersList.add(User.withUsername(member.get().getLogin())
+                              .password(passwordEncoder.encode(member.get().getPassword()))
+                              .roles("USER")
+                              .build());
+        }
+        if(manager.isPresent()){
+            usersList.add(User.withUsername(manager.get().getLogin())
+                              .password(passwordEncoder.encode(manager.get().getPassword()))
+                              .roles("MANAGER")
+                              .build());
+        }
+        usersList.add(User.withUsername("admin")
+                          .password(passwordEncoder.encode("admin"))
+                          .roles("ADMIN")
+                          .build());
         return usersList;
     }
 
     public UserDetails findUserByLogin(String login) {
-        return createUserList().stream()
-                               .filter(user -> user.getUsername()
-                                                   .equals(login))
-                               .findFirst()
-                               .orElseThrow(() -> new UsernameNotFoundException("No user was found"));
+        return createUserList(login).stream().findFirst()
+                                    .orElseThrow(() -> new UsernameNotFoundException("No user was found"));
     }
 
 }
