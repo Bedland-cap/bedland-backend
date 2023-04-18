ba347f69bbfa06c23846cf491abfcac15adaa42c
KarolinaZ-k <karolina_zagrodnik@wp.pl>
2023-04-18 17:57:45 +0000

    Merged in feature/BL-78-set-up-swagger-documentation-for-a (pull request #44)
    BL-78 set up swagger configuration
    
    * BL-78 set up swagger configuration
    
    
    Approved-by: Przemyslaw Szafranski

--- src/main/java/com/capgemini/bedland/security/SecurityConfig.java
+++ src/main/java/com/capgemini/bedland/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -15,6 +16,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -37,7 +39,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth/**")
+                .requestMatchers("/","/auth/**","/swagger-ui/**","/v3/api-docs/**")
                 .permitAll()
                 .requestMatchers(HttpMethod.GET, "/**")
                 .authenticated()
@@ -51,7 +53,10 @@ public class SecurityConfig {
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+
         return http.build();
     }
 
