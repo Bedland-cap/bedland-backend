b317a06e62c7eb960f103f956c55e3b85e1c6d51
Karolina Zagrodnik <karolina.zagrodnik@capgemini.com>
2023-04-28 13:38:37 +0200

    BL-227 fix bug and correct test with failed my maven install

--- src/main/java/com/capgemini/bedland/security/LogoutService.java
+++ src/main/java/com/capgemini/bedland/security/LogoutService.java
@@ -23,20 +23,17 @@ public class LogoutService implements LogoutHandler {
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        {
-            final String authHeader = request.getHeader("Authorization");
-            final String jwt;
-            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-                return;
-            }
-            jwt = authHeader.substring(7);
-            Optional<OwnerEntity> owner = ownerRepository.findByToken(jwt);
-            Optional<ManagerEntity> manager = managerRepository.findByToken(jwt);
-            owner.ifPresent(ownerEntity -> userDao.deleteTokenInDB(ownerEntity
-                                                                           .getId(), "ROLE_USER"));
-            manager.ifPresent(managerEntity -> userDao.deleteTokenInDB(managerEntity
-                                                                               .getId(), "ROLE_MANAGER"));
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
         }
+        jwt = authHeader.substring(7);
+        Optional<OwnerEntity> owner = ownerRepository.findByToken(jwt);
+        Optional<ManagerEntity> manager = managerRepository.findByToken(jwt);
+        owner.ifPresent(ownerEntity -> userDao.deleteTokenInDB(ownerEntity
+                                                                       .getId(), "ROLE_USER"));
+        manager.ifPresent(managerEntity -> userDao.deleteTokenInDB(managerEntity
+                                                                           .getId(), "ROLE_MANAGER"));
     }
-
 }
